3dc0f4a5cf04e8349629d1f8b6d7264057fa0489
Quoctv1508 <quoctv1508@gmail.com>
2025-04-22 21:18:39 +0700

    create project

---
+++ src/main/java/vn/alaxed/booksellingweb_backend/BooksellingwebBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.alaxed.booksellingweb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksellingwebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksellingwebBackendApplication.class, args);
+	}
+
+}
